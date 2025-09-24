a5a917f781892f7d43222d50196a604d60e75f29
jhonatanramo <jhonatanleonel666@gmai,com>
2025-09-24 19:02:27 -0400

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Index } from "./pages/Index";
-import { LandingPages } from "./pages/Landigpages"; // Ajustado al nombre correcto
 
 export function App() {
   return (
@@ -10,7 +9,6 @@ export function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/index" element={<Index />} />
-        <Route path="/la" element={<LandingPages />} />
         <Route path="*" element={<h1>404 - Página no encontrada</h1>} />
       </Routes>
     </BrowserRouter>
