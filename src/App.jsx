f52303b4a13dbf756e01394a5d0af569e9af0439
jhonatanramo <jhonatanleonel666@gmai,com>
2025-09-24 18:48:32 -0400

    Actualización1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Index } from "./pages/Index";
-import { LandingPages } from "./pages/landigpages";
+import { LandingPages } from "./pages/Landigpages"; // Ajustado al nombre correcto
+import { Prueba } from "./pages/P_reportes";
 
 export function App() {
   return (
@@ -10,6 +11,8 @@ export function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/index" element={<Index />} />
+        <Route path="/prueba" element={<Prueba />} />
+        <Route path="/landing" element={<LandingPages />} />
         <Route path="*" element={<h1>404 - Página no encontrada</h1>} />
       </Routes>
     </BrowserRouter>
